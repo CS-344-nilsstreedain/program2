9f28315458ff813fbf4ef4aa2b78b80f4e4ac06e
Nils Streedain <nils@streedain.com>
2023-02-05 01:43:27 -0800

    Getname/getEst refactoring

--- main.c
+++ main.c
@@ -17,63 +17,60 @@ void processFile(char *filename) {
 	printf("Created directory with name %s\n\n", "unknown");
 }
 
-void getEst(int getMin) {
-	char filename[256];
+void getEstFile(int mode, char* filename) {
 	DIR* currDir = opendir(".");
 	struct dirent *aDir;
-	off_t size = !getMin ? 0 : OFF_MAX;
+	off_t size = --mode ? OFF_MAX : 0;
 	struct stat dirStat;
 	
 	while ((aDir = readdir(currDir)) != NULL) {
 		if (strncmp(aDir->d_name,"movies_", 7) == 0 && strcmp(aDir->d_name + aDir->d_namlen - 4, ".csv") == 0) {
 			stat(aDir->d_name, &dirStat);
-
-			if ((!getMin && dirStat.st_size > size) || (getMin && dirStat.st_size < size)) {
+			
+			if ((!mode && dirStat.st_size > size) || (mode && dirStat.st_size < size)) {
 				size = dirStat.st_size;
-				memset(filename, '\0', sizeof(filename));
+				memset(filename, '\0', 256 * sizeof(char));
 				strcpy(filename, aDir->d_name);
 			}
 		}
 	}
 	closedir(currDir);
-	processFile(filename);
 }
 
-int getName(void) {
+// 1 = max
+// 2 = min
+// 3 = custom
+void getName(int mode) {
 	char filename[256];
-	printf("Enter the complete file name: ");
-	scanf("%s", filename);
-	
-	if (access(filename, F_OK) == -1) {
-		printf("The file %s was not found. Try again\n", filename);
-		return 1;
-	}
+	if (mode == 3) {
+		printf("Enter the complete file name: ");
+		scanf("%s", filename);
+		
+		if (access(filename, F_OK) == -1) {
+			printf("The file %s was not found. Try again\n", filename);
+			return;
+		}
+	} else
+		getEstFile(mode, filename);
 	processFile(filename);
-	return 0;
 }
 
 void fileToProcess(void) {
-	int userInput;
+	int input;
 	char fileNotFound = 1;
 	while (fileNotFound) {
 		printf("\nWhich file you want to process?\nEnter 1 to pick the largest file\nEnter 2 to pick the smallest file\nEnter 3 to specify the name of a file\n\nEnter a choice from 1 to 3: ");
-		scanf("%d", &userInput);
-		if (userInput == 1 || userInput == 2) {
-			getEst(userInput - 1);
-		} else if (userInput == 3) {
-			fileNotFound = getName();
-		} else {
-			printf("You entered an incorrect choice. Try again.");
-		}
+		scanf("%d", &input);
+		input > 0 && input < 4 ? getName(input) : printf("You entered an incorrect choice. Try again.");
 	}
 }
 
 int main(int argc, const char * argv[]) {
-	int userInput = 0;
-	while (userInput != 2) {
+	int input = 0;
+	while (input != 2) {
 		printf("1. Select file to process\n2. Exit the program\n\nEnter a choice 1 or 2: ");
-		scanf("%d", &userInput);
-		switch(userInput) {
+		scanf("%d", &input);
+		switch(input) {
 			case 1:
 				fileToProcess();
 			case 2:
